6553dc37d5eab042f0aa3c6f8a69d8764a41f367
Rajmouli123 <vermamanisha2611@gmail.com>
2024-03-01 20:00:47 +0530

    updated,adminlogin,registration

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns= [
     path("",views.index,name="index"),
     path("about/",views.about,name="about"),
-    path("contact/",views.contact,name="contact")
+    path("contact/",views.contact,name="contact"),
+    path("admin_login/",views.admin_login,name="admin_login"),
+    path("admin_registration/",views.admin_regi,name="admin_registration"),
+    path("admin_dash/",views.admin_dash,name="admin_dash")
     
 ]
+
--- projectApp/views.py
+++ projectApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .models import Contact, Quote
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 
 
@@ -35,3 +38,30 @@ def contact(request):
         quote=Quote(email=email,name=name,mobile=mobile)
         quote.save()
     return render(request,'contact.html')
+
+
+def admin_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('admin_dash')
+        else:
+            messages.error(request,'Invalid UserName Or Password')
+            return redirect('admin_login')
+    return render(request,'admin_login.html')
+
+def admin_regi(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        admin=User.objects.create_user(username,email,password)
+        admin.save()
+        return redirect('admin_login')
+    return render(request,'admin_regi.html')
+
+def admin_dash(request):
+    return render(request,'admin_dash.html')
